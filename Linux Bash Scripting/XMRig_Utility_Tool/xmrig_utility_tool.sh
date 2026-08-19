60a64668d4a0f2a1d0d274e7fb0cbe34947fac4c
Jackson Zenisek <115522316+JacksonZenisek@users.noreply.github.com>
2026-08-18 21:12:33 -0500

    Update xmrig_utility_tool.sh

--- Linux Bash Scripting/XMRig_Utility_Tool/xmrig_utility_tool.sh
+++ Linux Bash Scripting/XMRig_Utility_Tool/xmrig_utility_tool.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # A hopefully helpful utility tool for xmrig miners.
 # Developed by Jackson Zenisek
+# Last updated: 8/18/2026
 
 mainmenu() {
-echo "-------------------"
+echo "----------------------------"
 echo "XMRIG UTILITY TOOL"
-echo "-------------------"
+echo "Developed by Jackson Zenisek"
+echo "----------------------------"
 echo " "
 echo "Choose an option:"
 echo "1) Install XMRig"
 echo "2) Run XMRig"
+echo "3) Generate new Monero wallet"
+echo "4) View user guide"
 read mainmenselopt
-
-
 
 
 # Install XMRig option:
@@ -38,7 +40,7 @@ fi
 if ((installxmrigchoice==1)); then
 sleep 3
 printdir=$(pwd)
-echo "Are you sure you want to install XMRig in printdir ? y = 1/n = 2"
+echo "Are you sure you want to install XMRig in $printdir ? y = 1/n = 2"
 read doublechecker
 
 fi
@@ -66,6 +68,15 @@ fi
 
 
 if ((installxmrigchoice==2)); then
+echo " "
+echo " "
+showcurrentdirectory=$(pwd)
+echo "Here is your current directory:"
+echo "$showcurrentdirectory "
+echo " "
+showfolders=$(ls -d */)
+echo "Here are some nearby folder options:"
+echo "$showfolders"
 echo " "
 echo "Enter the directory that you want to install XMRig in:"
 read entereddir
@@ -171,6 +182,143 @@ read adminprev
 fi
 
 
+# Generate new Monero wallet option
+
+if ((mainmenselopt==3)); then
+printcurrentdirectory=$(pwd)
+echo "What would you like to do?:"
+echo "1) Tnstall the Monero wallet generator in this directory: $printcurrentdirectory"
+echo "2) Install Monero wallet generator in a different directory"
+echo "3) Run Monero wallet generator (If already installed)"
+read walletgendirchoice
+
+
+fi
+
+
+if ((mainmenselopt==4)); then
+echo " "
+echo " "
+echo "------------------------------"
+echo "XMRIG UTILITY TOOl: USER GUIDE"
+echo "------------------------------"
+echo " "
+echo " "
+echo "To answer option questions, enter the number that corresponds to the task you want to happen."
+echo "Example:"
+echo "1) Apple"
+echo "2) Banana"
+echo "3) Cherry"
+echo "If you want the Banana option, enter 2."
+echo " "
+echo " "
+echo "Yes and No prompts:"
+echo "-------------------"
+echo "For yes, enter 1"
+echo "For no, enter 2"
+echo " "
+echo " "
+echo "To cancel a task or quit the program entirely: CRTL + C"
+echo " "
+echo " "
+echo "If you want to save your wallet key to a file, when it prompts you to paste your key  look for something like this 'Generated new wallet: YOUR_KEY'"
+echo " "
+echo " "
+echo "Option direction map:"
+echo "---------------------"
+echo "START OF PROGRAM"
+echo "Main Menu; 1) Install XMRig, 2)Run XMRig, 3) Generate new Monero wallet, 4) View user guide"
+echo " "
+echo " "
+echo "Path for option 1"
+echo "1) Install XMRig ====>> Choose an option: 1) Install XMRig in current direrctory: $showcurrentdir, 2) Install XMRig in a different directory"
+echo " "
+echo "Path for option 2"
+echo "2) Run XMRig ====>> 1) Where did you install XMRig?: This directory: $getcurrentdir, 2) A different directory"
+echo " "
+echo "Path for option 3"
+echo "3) Generate new Monero wallet ====>> What would you like to do?: 1) Tnstall the Monero wallet generator in this directory: $printcurrentdirectory, 2) Install the wallet generator in a different direrctory, 3) Run Monero wallet generator (if already installed)"
+echo " "
+echo "Path for option 4"
+echo "Viewing user guide"
+echo " "
+fi
+
+
+
+
+
+if ((walletgendirchoice==1)); then
+echo " "
+echo "Preparing to download Monero wallet generator ..."
+sleep 3
+wget https://downloads.getmonero.org/linux64
+mkdir monero && tar -xjvf linux64 -C monero
+cd monero/*
+echo " "
+echo "Installation complete!"
+
+
+elif ((walletgendirchoice==2)); then
+echo " "
+showcurrentdirectory=$(pwd)
+echo "Here is your current directory:"
+echo "$showcurrentdirectory"
+echo " "
+showfolders=$(ls -d */)
+echo "Here are some nearby folder options:"
+echo "$showfolders"
+echo " "
+echo "Enter the directory that you want to install Monero wallet generator:"
+read walletgendir
+cd $walletgendir
+echo " "
+echo "Entering $walletgendir ..."
+sleep 2
+echo "Preparing to download Monero wallet generator ..."
+sleep 3
+wget https://downloads.getmonero.org/linux64
+mkdir monero && tar -xjvf linux64 -C monero
+cd monero/*
+echo " "
+echo "Installation complete!"
+
+
+elif ((walletgendirchoice==3)); then
+echo " "
+echo "Enter the directory that Monero wallet generator is installed in:"
+read installedmonerogendir
+cd $installedmonerogendir
+cd monero/*
+sudo ./monero-wallet-cli
+echo" "
+echo "Would you like to save your wallet key to a file? y = 1/n = 2:"
+read savekeychoice
+fi
+
+
+if ((savekeychoice==1)); then
+echo " "
+echo " Enter your kwallet key:"
+read vdfvdvddcvregehbe
+echo " "
+echo "What directory would you like to save the file?:"
+read savekeydirect
+cd $savekeydirect
+getdate=$(date)
+
+echo "$vdfvdvddcvregehbe" > "xmrgeneatedwallK_$getdate.txt"
+echo " "
+echo "Wallet key saved to $savekeydirect   xmrgeneatedwallK_$getdate.txt"
+
+elif ((savekeychoice==2)); then
+
+exit
+
+fi
+
+
+
 if ((adminprev==1)); then
 echo " "
 echo "Starting XMRig under admin..."
@@ -188,7 +336,9 @@ cd build
 ./xmrig -o gulf.moneroocean.stream:$portenter -u $walKy -p $minername
 
 fi
+
 }
+
 
 # Call Functions List
 mainmenu
